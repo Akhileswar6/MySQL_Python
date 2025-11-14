32d8cb05430a716010943e5bb908cc778c75e531
Akhileswar <akhilkamale@gmail.com>
2025-11-14 22:31:09 +0530

    updated code

--- 12_Drop_table.py
+++ 12_Drop_table.py
@@ -3,23 +3,25 @@
  
 # Example 1: Drop Table in MySQL using Python
 
-# importing required libraries
 import mysql.connector
- 
+
+# Connect to the studentdb database 
 dataBase = mysql.connector.connect(
   host ="localhost",
   user ="root",
   passwd ="Akhil@0109",
-  database = "gfg"
+  database = "studentdb"
 )
 
-# preparing a cursor object
+# Create a cursor object
 cursorObject = dataBase.cursor()
  
-query ="DROP TABLE Student;"
+sql_query ="DROP TABLE students"
 
-cursorObject.execute(query)
+cursorObject.execute(sql_query)
 dataBase.commit()
+
+print("✅ Table 'students' dropped successfully from 'studentdb' database.")
 
 # disconnecting from server
 dataBase.close()
